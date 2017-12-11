94eecad7b500fabb5aac3e2665c8d877227adf2e
luisaGonzales <luisagc711@gmail.com>
2017-12-11 16:15:41 -0500

    styles and store

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,7 +1,11 @@
 import createStore from 'redux-zero';
 
 const initialState = {
-    title: 'Woasdasdrks'
+    title: 'Create it Yourself', 
+    gender: null,
+    genderSelect: false, 
+    age: null,
+    ageSelected: false
 }
 
 const store = createStore(initialState);
