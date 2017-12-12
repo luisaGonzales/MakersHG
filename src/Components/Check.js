3b88e46bc3e9fddb8d87072a64ce1f15546a0884
DianaQuispe <danaalienmas@gmail.com>
2017-12-11 23:35:51 -0500

    add button

--- src/Components/Check.js
+++ src/Components/Check.js
@@ -1,7 +1,31 @@
 import React, {Component} from 'react';
+import {
+  Grid,
+  Row,
+  Col,
+  ButtonGroup,
+  Button,
+  DropdownButton,
+  MenuItem,
+  Image
+} from "react-bootstrap";
 
 export const Check = () => {
     return (
-        <div>Check</div>
+        <Grid>
+            <Row>
+                <Col sm={6} md={6}>
+
+                </Col>
+                <Col sm={6} md={6}>
+                <div>
+                    <h2>DETALLES</h2>
+                    <div className='colors'>
+                        <p>Color</p>
+                    </div>
+                </div>
+                </Col>
+            </Row>
+        </Grid>
     );
 }
