fd54b0a599043421aed2cf41816565929f83f9d4
DianaQuispe <danaalienmas@gmail.com>
2017-12-12 09:50:59 -0500

    add margin necesary

--- src/Components/Gender.js
+++ src/Components/Gender.js
@@ -5,7 +5,6 @@ import { Grid, Row, Col } from "react-bootstrap";
 export const Gender = () => {
     return (
       <Row>
-        <h2>¡Primero selecciona tu género!</h2>
         <Col md={6} sm={6} lg={6} xs={6}>
           <div
             className="selectGender-gender center-block img-responsive"
