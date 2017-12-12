a2f4ba7e8fd0453892c4a31cdfd3e4a6d2bed550
DianaQuispe <danaalienmas@gmail.com>
2017-12-12 15:49:31 -0500

    add changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ import {Layers} from './Components/Layers';
 import {Check} from './Components/Check';
 
 const App = ({title, genderSelect, ageSelected, age, sizeOptions, imgs, imgSelected, color, gender, size, ageTxt, colorOptions})=> {
-  return(
-    <HashRouter>
+  return <HashRouter>
       <Switch>
-          <Route exact path="/" render={() => <Menu title={title} genderSelect={genderSelect} ageSelected={ageSelected} />} />
-          <Route exact path="/menu" render={() => <Menu title={title} genderSelect={genderSelect} ageSelected={ageSelected} />} />
-          <Route exact path="/layers" render={() => <Layers size={size} age={age}  sizeOptions={sizeOptions} imgs={imgs} imgSelected={imgSelected} gender={gender} color={color} colorOptions={colorOptions}/>} />
-          <Route exact path="/check" render={() => <Check imgs={imgs} color={color} gender={gender} age={ageTxt} size={size} />} />
-          <Route render={() => <Redirect to={"/"} />}/>
+        <Route exact path="/" render={() => <Menu title={title} genderSelect={genderSelect} ageSelected={ageSelected} />} />
+        <Route exact path="/menu" render={() => <Menu title={title} genderSelect={genderSelect} ageSelected={ageSelected} />} />
+        <Route exact path="/layers" render={() => <Layers size={size} age={age} sizeOptions={sizeOptions} imgs={imgs} imgSelected={imgSelected} gender={gender} color={color} colorOptions={colorOptions} />} />
+        <Route exact path="/check" render={() => <Check colorOptions={colorOptions} imgs={imgs} color={color} gender={gender} imgSelected={imgSelected} age={ageTxt} size={size} />} />
+        <Route render={() => <Redirect to={"/"} />} />
       </Switch>
-    </HashRouter>)
+    </HashRouter>;
 }
 
 const mapToProps = ({title, genderSelect, ageSelected, age, sizeOptions, imgs, imgSelected, color, gender, size, ageTxt, colorOptions}) => ({title, genderSelect, ageSelected, age, sizeOptions, imgs, imgSelected, color, gender, size, ageTxt, colorOptions})
