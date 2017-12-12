48b5120207e2365abff44d2ab1049e56da19e735
luisaGonzales <luisagc711@gmail.com>
2017-12-11 22:24:00 -0500

    change view

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -8,12 +8,14 @@ const initialState = {
     ageSelected: false, 
     sizeOptions : [["XS", "S", "M", "L", "XL", "XXL", "XXXL"], ["10", "12", "14", "16"], ["3", "6", "9", "12", "24"]],
     size : null, 
+    color: "black",
     imgs : [
         "//image.spreadshirtmedia.net/image-server/v1/productTypes/812/views/1/appearances/2?width=350&height=350&mediaType=webp",
         "//image.spreadshirtmedia.net/image-server/v1/productTypes/812/views/2/appearances/2?width=350&height=350&mediaType=webp",
         "//image.spreadshirtmedia.net/image-server/v1/productTypes/812/views/3/appearances/2?width=350&height=350&mediaType=webp",
         "//image.spreadshirtmedia.net/image-server/v1/productTypes/812/views/4/appearances/2?width=350&height=350&mediaType=webp"
-    ]
+    ],
+    imgSelected : 0, 
 }
 
 const store = createStore(initialState);
